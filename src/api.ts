c3e96bc0fd64f53defa8e4b30cd258517dcea5d8
Oliver Shin <oliver.shin@eyevinn.se>
2023-11-07 14:27:46 +0100

    feat: api modelling base

--- src/api.ts
+++ src/api.ts
@@ -14,7 +14,11 @@ export interface HealthcheckOptions {
   title: string;
 }
 
-const healthcheck: FastifyPluginCallback<HealthcheckOptions> = (fastify, opts, next) => {
+const healthcheck: FastifyPluginCallback<HealthcheckOptions> = (
+  fastify,
+  opts,
+  next
+) => {
   fastify.get<{ Reply: Static<typeof HelloWorld> }>(
     '/',
     {
@@ -62,4 +66,4 @@ export default (opts: ApiOptions) => {
   // register other API routes here
 
   return api;
-}
+};
