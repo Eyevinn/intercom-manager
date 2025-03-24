641cb7754bc159be442970f21af6c93d949a1aac
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-24 12:55:57 +0100

    feat: can determine OSC environment from OSC_HOSTNAME

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,16 @@
 if [ ! -z "$OSC_HOSTNAME" ]; then
   export PUBLIC_HOST="https://$OSC_HOSTNAME"
   echo "Setting PUBLIC_HOST to $PUBLIC_HOST"
+  
+  # Determine environment from hostname
+  if [[ "$OSC_HOSTNAME" == *"-dev."* ]]; then
+    export OSC_ENVIRONMENT="dev"
+  elif [[ "$OSC_HOSTNAME" == *"-stage."* ]]; then
+    export OSC_ENVIRONMENT="stage"
+  elif [[ "$OSC_HOSTNAME" == *".prod."* ]]; then
+    export OSC_ENVIRONMENT="prod"
+  fi
+  
   if [ -z "$OSC_ACCESS_TOKEN" ]; then
     echo "OSC_ACCESS_TOKEN is not set. Limited functionality will be available."
   fi
