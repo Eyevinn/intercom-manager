3176cd006909b90ddf3db3b407b82e3565f17116
Max Andreasen <maxandr@kth.se>
2025-10-24 15:47:36 +0200

    fix: removed misspelling in message

--- src/api_re_auth.ts
+++ src/api_re_auth.ts
@@ -49,7 +49,7 @@ const apiReAuth: FastifyPluginCallback = (fastify, _, next) => {
           });
         }
       } else {
-        reply.code(405).send({ error: 'No OSC_ACCESS_TOKEN token set, method not allowed' });
+        reply.code(405).send({ error: 'No OSC_ACCESS_TOKEN set, method not allowed' });
       }
     }
   );
