e9762d034eac87b955512683bd969d80ec0955b5
Rohan <71641133+Rohanhos@users.noreply.github.com>
2025-12-11 13:14:55 +0100

    feat: create indexes for getSessionsByQuery for CouchDB (#180)
    * feat: create indexes for getSessionsByQuery
    
    Co-Authored-By: zeynepcaliskan <143728674+zeynepcaliskan@users.noreply.github.com>
    
    * fix: remove try-catch block
    
    Co-Authored-By: zeynepcaliskan <143728674+zeynepcaliskan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: zeynepcaliskan <143728674+zeynepcaliskan@users.noreply.github.com>

--- src/db/couchdb.ts
+++ src/db/couchdb.ts
@@ -29,6 +29,7 @@ export class DbManagerCouchDb implements DbManager {
       this.nanoDb = this.client.db.use(
         this.dbConnectionUrl.pathname.replace(/^\//, '')
       );
+      await this.ensureSessionIndexes();
       this.sessionPruneInterval();
     }
   }
@@ -436,5 +437,50 @@ export class DbManagerCouchDb implements DbManager {
     const selector: any = { ...q };
     const response = await this.nanoDb.find({ selector, limit: 10000 }); // limit to 10000 sessions being queried
     return response.docs as unknown as UserSession[]; // could also expand type UserSession to avoid unknown
+  }
+
+  async ensureSessionIndexes(): Promise<void> {
+    if (!this.nanoDb) {
+      throw new Error('Database not connected');
+    }
+    // index for toInactivate, toReactivate, toExpire
+    await (this.nanoDb as any).createIndex({
+      index: {
+        fields: ['isExpired', 'isActive']
+      },
+      name: 'idx_isExpired_isActive',
+      ddoc: 'idx_isExpired_isActive',
+      type: 'json'
+    });
+
+    // index for getUsersForLine()
+    await (this.nanoDb as any).createIndex({
+      index: {
+        fields: ['isWhip', 'isExpired']
+      },
+      name: 'idx_isWhip_isExpired',
+      ddoc: 'idx_isWhip_isExpired',
+      type: 'json'
+    });
+
+    // index for getUsersForLine()
+    await (this.nanoDb as any).createIndex({
+      index: {
+        fields: ['productionId', 'lineId', 'isExpired']
+      },
+      name: 'idx_prod_line_isExpired',
+      ddoc: 'idx_prod_line_isExpired',
+      type: 'json'
+    });
+
+    // index for getActiveUsers()
+    await (this.nanoDb as any).createIndex({
+      index: {
+        fields: ['productionId', 'isActive']
+      },
+      name: 'idx_prod_isActive',
+      ddoc: 'idx_prod_isActive',
+      type: 'json'
+    });
   }
 }
