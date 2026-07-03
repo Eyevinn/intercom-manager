9fbdcee03c98d0d5cf0a74d73a9b5feda81c92db
sala07 <Sandra.larsson@eyevinn.se>
2026-07-03 14:35:27 +0200

    chore: solve test fail

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['./jest.setup.js']
+  setupFiles: ['./jest.setup.js'],
+  transformIgnorePatterns: ['node_modules/(?!uuid)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    'node_modules/uuid/.+\\.js$': 'ts-jest'
+  }
 };
